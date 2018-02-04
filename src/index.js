36249b3bb7c1077188eda521633a0c9919d5a96e
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2018-02-04 23:49:06 +0900

    [WIP] added the text search function that moved from pdfanno

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import * as annoListDropdown from './components/annoListDropdown'
 import * as downloadButton from './components/downloadButton'
 import * as labelInput from './components/labelInput'
 import * as uploadButton from './components/uploadButton'
+import * as searchUI from './components/searchUI'
 import * as ui from './uis'
 import * as event from './events'
 import * as util from './utils'
@@ -21,6 +22,7 @@ export {
     downloadButton,
     labelInput,
     uploadButton,
+    searchUI,
     ui,
     event,
     util
