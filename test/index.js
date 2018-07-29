151c164f8085a525145664c26d7080f2f6c46e23
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2018-07-29 23:33:18 +0900

    changed thet test order as temporary, because interdependencies between tests can not be resolved

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-import LabelInputReader from './components/labelInput/reader.unittest.js'
-import LabelInputBehavior from './components/labelInput/behavior.unittest.js'
-import LabelInputEditButton from './components/labelInput/editButton.unittest.js'
 import LabelInputDb from './components/labelInput/db.unittest.js'
+import LabelInputReader from './components/labelInput/reader.unittest.js'
+import LabelInputEditButton from './components/labelInput/editButton.unittest.js'
+import LabelInputBehavior from './components/labelInput/behavior.unittest.js'
